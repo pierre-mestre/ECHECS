bf9454cf11122ef1bef0c9ffe83b301bcbac2d28
Pierre Mestre <pierre.mestre@outlook.fr>
2020-01-23 16:56:12 +0100

    fin opti N°2

---
+++ AUTRE ANCIEN/MISE A JOUR DOSSIER.bat
@@ -0,0 +1,3 @@
+git pull
+echo DOSSIER MIS A JOUR
+pause
---
+++ tscp_project/tscp_project/data.h
@@ -0,0 +1,46 @@
+/*
+ *	DATA.H
+ *	Tom Kerrigan's Simple Chess Program (TSCP)
+ *
+ *	Copyright 1997 Tom Kerrigan
+ */
+
+
+/* this is basically a copy of data.c that's included by most
+   of the source files so they can use the data.c variables */
+
+extern int color[64];
+extern int piece[64];
+extern int side;
+extern int xside;
+extern int castle;
+extern int ep;
+extern int fifty; // si 50 coups joués
+extern int hash;
+extern int ply; // 
+extern int hply;
+extern gen_t gen_dat[GEN_STACK]; // coup a joué à une profondeur n
+extern int first_move[MAX_PLY];
+extern int history[64][64]; // méthode d'élagage
+extern hist_t hist_dat[HIST_STACK];
+extern int max_time; // temps alloué de réflexion
+extern int max_depth;
+extern int start_time;
+extern int stop_time;
+extern int nodes;
+extern move pv[MAX_PLY][MAX_PLY]; // stocke les coups sélectionnés
+extern int pv_length[MAX_PLY];
+extern BOOL follow_pv;
+extern int hash_piece[2][6][64];
+extern int hash_side;
+extern int hash_ep[64];
+extern int mailbox[120];
+extern int mailbox64[64];
+extern BOOL slide[6];
+extern int offsets[6];
+extern int offset[6][8];
+extern int castle_mask[64];
+extern char piece_char[6];
+extern int init_color[64];
+extern int init_piece[64];
+extern int can_attack[6][64][64] ; // canAttack[TypeDePiece][Source][Destination]
